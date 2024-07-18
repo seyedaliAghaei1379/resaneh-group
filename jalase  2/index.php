70d9c1a2b8d33736533140aa53cecd0ee010c5d3
seyedali <seyedali@gmail.com>
2024-07-18 07:06:44 -0700

    jalase 2 php

---
+++ jalase  2/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+// echo "hi";
+
+$name  = "ali";
+
+$family = "aghaei";
+
+$fullName = $name . " " . $family;
+
+
+
+$name .= " reza";
+
+
+echo $name;
+
+
+
+
+?>
+
+<script>
+
+    
+</script>
