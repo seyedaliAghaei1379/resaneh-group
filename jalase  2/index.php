288648fe3e275b4ca0b4e2f234cc970c7ab1b9e9
seyedali <seyedali@gmail.com>
2024-07-18 07:37:37 -0700

    jalase 2

--- jalase  2/index.php
+++ jalase  2/index.php
@@ -2,18 +2,72 @@
 
 // echo "hi";
 
-$name  = "ali";
+// $name  = " ";
 
-$family = "aghaei";
+// $family = "aghaei";
 
-$fullName = $name . " " . $family;
-
-
-
-$name .= " reza";
+// $fullName = $name . " " . $family;
 
 
-echo $name;
+
+// $name .= " reza";
+
+
+// echo $name;
+
+
+
+$output = null;
+
+$num1 = 10;
+$num2 = 20;
+
+
+$output = $num1 + $num2;
+$output = $num1 - $num2;
+$output = $num1 / $num2;
+$output = $num1 * $num2;
+$output = 10 * 5;
+$output = 10 - 5;
+$output = 10 / 5;
+
+$num3 = 10;
+$num3 += 6;
+$num3 -= 2;
+$num3 *= 2;
+$num3 /= 2;
+
+$output = $num3;
+
+
+
+
+$output = rand(1,10);
+
+
+// Rounds a  floating-point number
+$output = round(6.5); // => 7
+
+
+// Rounds a  floating-point number up
+$output = ceil(4.3); 
+$output = ceil(1.3); 
+
+
+// Rounds a  floating-point number down
+$output = floor(4.8);
+
+
+$output = sqrt(64);
+$output = abs(-64);
+$output = min([1,2,3]);
+$output = number_format(1235454.1212121 , 2 );
+
+
+
+
+
+echo $output;
 
 
 
